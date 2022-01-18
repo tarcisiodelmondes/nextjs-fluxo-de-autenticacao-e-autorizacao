0ae977914fb8a04986957bbe05acdb7a909a00dc
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-01-18 17:01:07 -0300

    feat: refreshToken

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,13 @@
-import { createContext, ReactNode } from "react";
+import Router from "next/router";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { destroyCookie, parseCookies, setCookie } from "nookies";
 import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 type SignInCredentials = {
   email: string;
@@ -9,16 +17,40 @@ type SignInCredentials = {
 type AuthContextData = {
   signIn: (credentials: SignInCredentials) => Promise<void>;
   isAuthenticated: boolean;
+  user: User;
 };
 
 type AuthProviderProps = {
   children: ReactNode;
 };
 
+export function signOut() {
+  destroyCookie(undefined, "nextAuth.token");
+  destroyCookie(undefined, "nextAuth.refreshToken");
+
+  Router.push("/");
+}
+
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { "nextAuth.token": token } = parseCookies();
+
+    if (token) {
+      api
+        .get("/me")
+        .then((response) => {
+          const { permissions, roles, email } = response.data;
+
+          setUser({ email, permissions, roles });
+        })
+        .catch(() => signOut());
+    }
+  }, []);
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
@@ -27,14 +59,33 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
         password,
       });
 
-      console.log(data);
+      const { permissions, roles, token, refreshToken } = data;
+
+      setCookie(undefined, "nextAuth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+      setCookie(undefined, "nextAuth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      Router.push("/dashboard");
     } catch (error) {
       console.log(error);
     }
   }
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, signIn }}>
+    <AuthContext.Provider value={{ isAuthenticated, signIn, user }}>
       {children}
     </AuthContext.Provider>
   );
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,90 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { destroyCookie, parseCookies, setCookie } from "nookies";
+import { signOut } from "../context/AuthContext";
+
+let cookies = parseCookies();
+let isRefreshing = false;
+
+let failedRequestsQueue = [];
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
+  headers: {
+    Authorization: `Bearer ${cookies["nextAuth.token"]}`,
+  },
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error.response.status === 401) {
+      if (error.response.data?.code === "token.expired") {
+        cookies = parseCookies();
+
+        const { "nextAuth.refreshToken": refreshToken } = cookies;
+
+        const originalConfig = error.config;
+
+        if (!isRefreshing) {
+          isRefreshing = true;
+
+          api
+            .post("/refresh", {
+              refreshToken,
+            })
+            .then((response) => {
+              const { token } = response.data;
+
+              setCookie(undefined, "nextAuth.token", token, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: "/",
+              });
+
+              setCookie(
+                undefined,
+                "nextAuth.refreshToken",
+                response.data.refreshToken,
+                {
+                  maxAge: 60 * 60 * 24 * 30, // 30 days
+                  path: "/",
+                }
+              );
+
+              api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+              failedRequestsQueue.forEach((request) =>
+                request.onSuccess(token)
+              );
+              failedRequestsQueue = [];
+            })
+            .catch((err: AxiosError) => {
+              failedRequestsQueue.forEach((request) => request.onFailure(err));
+              failedRequestsQueue = [];
+            })
+            .finally(() => {
+              isRefreshing = false;
+            });
+        }
+
+        return new Promise((resolver, reject) => {
+          failedRequestsQueue.push({
+            onSuccess: (token: string) => {
+              originalConfig.headers["Authorization"] = `Bearer ${token}`;
+
+              resolver(api(originalConfig));
+            },
+            onFailure: (err: AxiosError) => {
+              reject(err);
+            },
+          });
+        });
+      } else {
+        signOut();
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
